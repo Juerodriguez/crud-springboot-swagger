259e3847ea8f73745eaa4ffd26fafa1f0e972c13
Rodriguez Juan <stylorj@gmail.com>
2024-01-13 20:16:08 -0300

    Agregado de Documentador Swagger3 con el nuevo formato OAS3

---
+++ README.md
@@ -0,0 +1,6 @@
+# Crud SpringBoot
+Este es un proyecto de practica para crear una API Rest con Springboot y documentada con Swagger.
+
+Una vez corriendo el servicio entrar en el siguiente link para ver la documentación:
+
+http://localhost:9090/swagger-ui/index.html
--- src/main/java/com/juerodriguez/pruebaspring/controllers/UsuarioControl.java
+++ src/main/java/com/juerodriguez/pruebaspring/controllers/UsuarioControl.java
@@ -2,12 +2,15 @@ package com.juerodriguez.pruebaspring.controllers;
 
 import com.juerodriguez.pruebaspring.dao.UserDAO;
 import com.juerodriguez.pruebaspring.model.Usuario;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@Tag(name = "Controlador de Usuario", description = "Una api para mostrar un Spring Boot Rest CRUD")
 @RestController
 @RequestMapping("user")
 public class UsuarioControl {
@@ -15,6 +18,9 @@ public class UsuarioControl {
     @Autowired
     private UserDAO userDAO;
 
+    @Operation(
+            summary = "Obtención de usuario por ID",
+            description = "Escribir solamente el id")
     @GetMapping(value = "{user_id}")
     public ResponseEntity<Usuario> getUsuario(@PathVariable("user_id") Integer user_id){
         Optional<Usuario> optionalUsuario= userDAO.findById(user_id);
@@ -25,6 +31,9 @@ public class UsuarioControl {
         }
     }
 
+    @Operation(
+            summary = "Crear usuario",
+            description = "Escribir todos los campos, el campo id no es obligatorio ya que se auto genera")
     @PostMapping
     public ResponseEntity<Usuario> postUsuario(@RequestBody Usuario usuario){
         Usuario newuser = userDAO.save(usuario);
